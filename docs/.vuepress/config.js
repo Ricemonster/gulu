8b08c8081a05c29fa1e878a702cd1a0b85b9753f
Ricemonster <766140773@qq.com>
2019-07-25 00:26:41 +0800

    updata

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,38 @@
 module.exports = {
   base:'/gulu/',
-  title: '轱辘-UI',
-  description: '一个简单的 UI 框架',
+  title: 'VueGuo-UI',
+  description: '一个基于 Vue 的UI框架',
   markdown: {
     lineNumbers: true
   },
   themeConfig: {
-      sidebar: [
-          {
-              title: '入门',
-              collapsable: false,
-              children: [
-                  '/install/',
-                  '/get-started/'
-              ]
-          },
-          {
-              title: '组件',
-              children: [ 
-                  '/components/button',
-                  '/components/input',
-                  '/components/layout',
-                  '/components/tabs',
-                  '/components/toast',
-                  '/components/popover',
-                  '/components/collapse',
-                  '/components/Grid'
-              ]
-          }
-      ]
+      nav: [
+        { text: '指南', link: '/guide/' },
+        { text: '欢迎 Star', link: 'https://github.com/Ricemonster/VueGuo-UI' },
+        { text: 'GitHub', link: 'https://github.com/ricemonster' },
+      ],
+//       sidebar: [
+//           {
+//               title: '入门',
+//               collapsable: false,
+//               children: [
+//                   '/install/',
+//                   '/get-started/'
+//               ]
+//           },
+//           {
+//               title: '组件',
+//               children: [ 
+//                   '/components/button',
+//                   '/components/input',
+//                   '/components/layout',
+//                   '/components/tabs',
+//                   '/components/toast',
+//                   '/components/popover',
+//                   '/components/collapse',
+//                   '/components/Grid'
+//               ]
+//           }
+//       ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,14 @@
-# 轱辘 UI 官网
-
-轱辘 UI 是一个好用的 UI 框架，提供了 ..... 等常用组件，适合移动端和PC 端的使用。
+---
+home: true
+heroImage: /logo.png
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 简洁至上
+  details: 以最少的代码构建整个UI界面。简洁的代码，简洁的设计，高质量的使用，是我们一贯的追求
+- title: Vue驱动
+  details: UI框架基于Vue开发，和Vue项目搭配使用，享受 Vue + VueGuo-UI 的开发体验
+- title: 持续集成
+  details: 自动化测试，保证每个组件质量。完整覆盖组件，以保证完美运行，每一次上传都确保无误后，方才更新，确保质量
+footer: MIT Licensed | Copyright © 2019-present Ricemonster
+---
